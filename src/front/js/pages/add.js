de3f68badb61daea72698b8c44925cef39dde934
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2025-01-02 20:31:49 +0000

    change

--- src/front/js/pages/add.js
+++ src/front/js/pages/add.js
@@ -114,7 +114,7 @@ export const Add = () => {
             className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           />
         </div>
-        <button
+         <button
           type="submit"
           className="w-full py-3 px-4 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 "
         >
