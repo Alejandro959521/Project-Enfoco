4939aa0250fd22bea3038d3b8ed896521f032d76
Miguel Ojeda <cabrerasm081@gmail.com>
2026-08-10 13:01:13 -0400

    agregado de size en  webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,11 @@ const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
 module.exports = merge(common, {
     mode: 'production',
+    performance: {
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
+    hints: false
+  },
     output: {
         publicPath: '/'
     },
